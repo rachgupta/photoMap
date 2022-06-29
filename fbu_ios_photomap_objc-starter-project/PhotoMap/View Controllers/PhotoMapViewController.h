285d75c01baa8d09b8f3bb34e88c973a7ad67aec
Rachna Gupta <rachnagupta@fb.com>
2022-06-29 10:34:08 -0700

    finished PhotoMap xcode project

---
+++ fbu_ios_photomap_objc-starter-project/PhotoMap/View Controllers/PhotoMapViewController.h
@@ -0,0 +1,15 @@
+//
+//  PhotoMapViewController.h
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface PhotoMapViewController : UIViewController
+@property (nonatomic, strong) UIImage *selectedImage;
+
+@end
